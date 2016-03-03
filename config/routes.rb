dbe36fbd92ab3415ef100fd64701490042cf3353
vanhensy <anhvynv.vanhensy@gmail.com>
2016-03-03 07:54:37 +0700

    Adding Tagging Feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :articles, path: ""
   resources :articles
-     resources :tags
+  resources :articles
+  resources :tags
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
