6f62fa212eb2aab78890e54474637f3f77d74a18
vanhensy <anhvynv.vanhensy@gmail.com>
2016-03-03 07:42:45 +0700

    Adding Tagging Feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,20 @@
 class Article < ActiveRecord::Base
-  
+  has_many :taggings
+  has_many :tags, through: :taggings
   # It returns the articles whose titles contain one or more words that form the query
   def self.search(query)
     # where(:title, query) -> This would return an exact match of the query
     where("title like ?", "%#{query}%") 
   end
+ def tag_list
+  self.tags.collect do |tag|
+    tag.name
+  end.join(", ")
+end
+    def tag_list=(tags_string)
+        tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+  new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+  self.tags = new_or_found_tags
+end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229192854) do
+ActiveRecord::Schema.define(version: 20160303000002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,4 +23,22 @@ ActiveRecord::Schema.define(version: 20160229192854) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "taggings", ["article_id"], name: "index_taggings_on_article_id", using: :btree
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "taggings", "articles"
+  add_foreign_key "taggings", "tags"
 end
